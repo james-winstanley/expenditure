01ec44c8670eaef6edd51145c981d911fff78a59
James Winstanley <3266204+JamesWinstanley270@users.noreply.github.com>
2022-01-29 10:07:42 +0000

    command query skeleton

--- src/Winstanley.Expenditure.Database/Entities/PaymentItem.cs
+++ src/Winstanley.Expenditure.Database/Entities/PaymentItem.cs
@@ -8,13 +8,37 @@ public record PaymentItem
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Amount { get; set; }
+    public DateOnly DueDate { get; set; }
     public int SortOrder { get; set; }
+    public int? PersonId { get; set; }
     public int GroupId { get; set; }
     public int PaymentFrequencyId { get; set; }
 
+
+    /// <summary>
+    /// Gets the person.
+    /// A payment item can belong to a person.
+    /// If not assigned to a person, this payment item belongs to the household.
+    /// </summary>
+    /// <value> The person. </value>
+    [ForeignKey(nameof(PersonId))]
+    public Person? Person { get; }
+
+
+    /// <summary>
+    /// Gets the group.
+    /// All payment items belong to a group.
+    /// </summary>
+    /// <value> The group. </value>
     [ForeignKey(nameof(GroupId))]
     public Group Group { get; }
 
+
+    /// <summary>
+    /// Gets the payment frequency.
+    /// By default, most payments are given a monthly payment frequency.
+    /// </summary>
+    /// <value> The payment frequency. </value>
     [ForeignKey(nameof(PaymentFrequencyId))]
     public PaymentFrequency PaymentFrequency { get; }
 }
